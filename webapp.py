0f1f42442b253c6733dc2cb5206a8888f9153e44
savaxxahx <savannah@katherynsavannah.com>
2017-10-10 09:59:49 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -8,13 +8,25 @@ def render_main():
     
 @app.route("/dollarstoyen")
 def render_dollarstoyen():  
-   money = float(request.args['money']) *112.15
+   money = float(request.args['money'])*112.15
    reply: "Your amount in Japanese Yen is " + money 
    
    return render_template('dollarstoyen.html', response = reply)
   
     
-    
+@app.route("/fahrenheittocelsuis")
+def render_fahrenheittocelsuis():
+    temp = (float(request.args['temp'])-32)/1.8
+    reply: " Your temperature in Celsuis is" + temp
+    return render_template('fahrenheittocelsuis.html', response = reply)
+
+@app.route("/inchestocenti")
+def render_inchestocenti():
+    height = float(request.args['height'])
+    reply: " Your height in centimeters is" + height
+     return render_template('inchestocenti.html', response = reply)
+
+
     
     if __name__=="__main__":
     app.run(debug=False, port=54321)
